2934d669fe2bacb38352ba3bfc90d6557a121055
Swiggityy <Chris_Nguyen@csu.fullerton.edu>
2018-05-16 14:18:13 -0700

    updated names

---
+++ HMWorld/HMWorld/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace HMWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.txtCityName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(82, 35);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Manager";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(12, 12);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(55, 13);
+            this.lblResults.TabIndex = 1;
+            this.lblResults.Text = "City Name";
+            // 
+            // txtCityName
+            // 
+            this.txtCityName.Location = new System.Drawing.Point(73, 9);
+            this.txtCityName.Name = "txtCityName";
+            this.txtCityName.Size = new System.Drawing.Size(100, 20);
+            this.txtCityName.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtCityName);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.TextBox txtCityName;
+    }
+}
+
